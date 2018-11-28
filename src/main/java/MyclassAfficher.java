7a4eb6bfad588bb98cb8c88e2456a5820eff0773
Your Name <you@example.com>
2018-11-28 08:35:33 +0100

    reinitialized files

---
+++ src/main/java/MyclassAfficher.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+
+public class MyclassAfficher {
+
+	public void afficherMsg() {
+		
+		System.out.println("« Welcome to IC »");
+	}
+}
---
+++ src/test/java/AfficherTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AfficherTest {
+
+	@Test
+	public void testAfficherMsg() {
+MyclassAfficher myClass = new MyclassAfficher();
+myClass.afficherMsg();}
+
+}
